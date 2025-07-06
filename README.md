00c71f7b5827ced63a79618a9ac4f8c3b6f427fa
char1esss <char1esssforreal@gmail.com>
2025-07-06 20:48:19 +0800

    completed quiz 2 and initialize amour_v2

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 其实写markdown也是**学习**的一部分
 
 ## quiz 1:
-quiz 1其实是一个**aplha beta滤波器**的题目，用来对测量的数据进行平滑处理，以达到拟合理想实验数据的效果。
+一个**aplha beta滤波器**的题目，用来对测量的数据进行平滑处理，以达到拟合理想实验数据的效果。<br>
 alpha beta滤波器介绍: [Wikipedia](https://en.wikipedia.org/wiki/Alpha_beta_filter)
 
 ## amour_v1:
 装甲板识别，[参考代码](https://blog.csdn.net/qq_40403096/article/details/107039702)。<br>
 注：只能在**amour_1.jpg**上**完美**运行，判断条件仍需完善。
+
+## quiz 2:
+弹道计算问题。建立好模型之后，直接迭代求解即可。
--- amour_v1/amour.cpp
+++ amour_v1/amour.cpp
@@ -27,17 +27,15 @@ public:
 int main()
 {
     Mat image = imread("amour_1.jpg"), binary, Gaussian, dilatee;
-    Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
     vector<vector<Point>> contours;
     vector<Vec4i> hierarchy;
     vector<Mat> channels;
 
     split(image, channels);
 
-    threshold(channels[0], binary, 240, 255, 0);                              // 二值化
-    GaussianBlur(binary, Gaussian, Size(5, 5), 0);                            // 滤波
-    dilate(Gaussian, dilatee, element);                                       // 扩大灯条面积
-    findContours(dilatee, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE); // 轮廓检测
+    threshold(channels[0], binary, 245, 255, 0);                               // 二值化
+    GaussianBlur(binary, Gaussian, Size(3, 3), 0);                             // 滤波
+    findContours(Gaussian, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE); // 轮廓检测
 
     vector<LightDescriptor> lightInfos; // 创建一个灯条类的动态数组
 
@@ -46,17 +44,18 @@ int main()
     {
         // 求轮廓面积
         double area = contourArea(contours[i]);
-        // 去除较小轮廓&fitEllipse的限制条件
-        if (area < 5 || contours[i].size() <= 1)
+        // 除去较小轮廓
+        if (area < 100 || contours[i].size() <= 1 || area > 1000)
             continue; // 相当于就是把这段轮廓去除掉
         // 用椭圆拟合区域得到外接矩形（特殊的处理方式：因为灯条是椭圆型的，所以用椭圆去拟合轮廓，再直接获取旋转外接矩形即可）
-        RotatedRect Light_Rec = fitEllipse(contours[i]);
+        RotatedRect Light_Rec = minAreaRect(contours[i]);
 
         // 长宽比和轮廓面积比限制（由于要考虑灯条的远近都被识别到，所以只需要看比例即可）
-        if (Light_Rec.size.width / Light_Rec.size.height > 2)
-            continue;
+        // if (Light_Rec.size.width / Light_Rec.size.height > 2)
+        //     continue;
         lightInfos.push_back(LightDescriptor(Light_Rec));
     }
+    
     // 二重循环多条件匹配灯条
     for (size_t i = 0; i < lightInfos.size(); i++)
     {
@@ -68,7 +67,7 @@ int main()
             // 由于灯条长度会因为远近而受到影响，所以按照比值去匹配灯条
             float LenGap_ratio = abs(leftLight.length - rightLight.length) / max(leftLight.length, rightLight.length);
             float dis = pow(pow((leftLight.center.x - rightLight.center.x), 2) + pow((leftLight.center.y - rightLight.center.y), 2), 0.5);
-            float meanLen = (leftLight.length + rightLight.length) / 2;
+            float meanLen = (leftLight.width + rightLight.width) / 2;
             float lengap_ratio = abs(leftLight.length - rightLight.length) / meanLen;
             float yGap = abs(leftLight.center.y - rightLight.center.y);
             float yGap_ratio = yGap / meanLen;
@@ -76,24 +75,21 @@ int main()
             float xGap_ratio = xGap / meanLen;
             float ratio = dis / meanLen;
             float length_ratio = leftLight.length / rightLight.length;
-            float light_ratio_residual = abs(leftLight.length / leftLight.width - rightLight.length / rightLight.width );
             // 匹配不通过的条件
-            if (angleGap_ > 20 ||
+            if (angleGap_ > 15 ||
                 LenGap_ratio > 2 ||
                 lengap_ratio > 0.5 ||
                 yGap_ratio > 1.5 ||
                 xGap_ratio > 2 ||
                 xGap_ratio < 0.8 ||
-                ratio > 2.5 ||
-                ratio < 0.90 ||
-                light_ratio_residual > 2
-            )
+                ratio > 2 ||
+                ratio < 0.90)
             {
                 continue;
             }
             // 绘制矩形
             Point center = Point((leftLight.center.x + rightLight.center.x) / 2, (leftLight.center.y + rightLight.center.y) / 2);
-            RotatedRect rect = RotatedRect(center, Size(dis, meanLen), (leftLight.angle + rightLight.angle) / 2);
+            RotatedRect rect = RotatedRect(center, Size(meanLen, dis), (leftLight.angle + rightLight.angle) / 2);
             Point2f vertices[4];
             rect.points(vertices);
             for (int i = 0; i < 4; i++)
@@ -101,7 +97,6 @@ int main()
                 putText(image, to_string(i + 1), vertices[i], FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
                 line(image, vertices[i], vertices[(i + 1) % 4], Scalar(0, 0, 255), 2.2);
             }
-            lightInfos.erase(lightInfos.begin() + i);
         }
     }
 
